0be1e0e3ff6f7b4a29e6caf943f0c9217a367236
emergentjamesdm <jamesdm@emergentsoftware.net>
2018-11-15 16:40:34 -0600

    Ideas

--- ScreenTime/MainWindow.xaml.cs
+++ ScreenTime/MainWindow.xaml.cs
@@ -39,7 +39,16 @@ namespace ScreenTime
                 Interval = 5000
             };
             Timer.Elapsed += TimerElapsed;
-            Timer.Start();            
+            Timer.Start();           \
+            
+
+//            Screen Time
+//Chromes secondary Id needs to be the window title, that will tell us which tab is active
+//Applications Used
+//Time Worked Today
+//Day Started?
+//Day Ended?
+//Breaks ?
         }
 
         private void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
